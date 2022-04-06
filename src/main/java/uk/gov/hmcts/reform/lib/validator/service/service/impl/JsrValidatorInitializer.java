4be270d8773e7f3c3090fe3424602442bc0e9822
arshinsalim <arshin.salim@hmcts.net>
2022-04-06 12:26:17 +0100

    updated for common library

--- src/main/java/uk/gov/hmcts/reform/lib/validator/service/service/impl/JsrValidatorInitializer.java
+++ src/main/java/uk/gov/hmcts/reform/lib/validator/service/service/impl/JsrValidatorInitializer.java
@@ -1,7 +1,6 @@
 package uk.gov.hmcts.reform.lib.validator.service.service.impl;
 
-import static net.logstash.logback.encoder.org.apache.commons.lang3.BooleanUtils.isNotTrue;
-
+import static org.apache.commons.lang3.BooleanUtils.isNotTrue;
 import javax.annotation.PostConstruct;
 import javax.validation.ConstraintViolation;
 import javax.validation.Validation;
